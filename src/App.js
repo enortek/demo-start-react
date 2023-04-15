a7f615c02bcc1f0327f7892a29a4be313f3cc821
enortek <enortek@gmail.com>
2023-04-15 05:31:52 +0200

    more updates to app.js&.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='container'>
-      <h1>Pokemon Search</h1>
-      <div>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Bulbasur</td>
-            <td>Grass, Poison</td>
-          </tr>
-        </tbody>
+      <div className='container__title'>
+        <h1>Pokemon Search</h1>
+        <div className='container__search'>
+          <input
+            type='text'
+            placeholder='Search Pokemon'
+          />
+          <button>Search</button>
+        </div>
+        <div className='container__table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Bulbasur</td>
+              <td>Grass, Poison</td>
+            </tr>
+          </tbody>
+        </div>
       </div>
     </div>
   );
